c0715a8d8812f53fe290db998cbaf8e9ccfdfa6b
terzeron <terzeron@gmail.com>
2020-10-23 09:17:42 +0900

    add an example for functional interface

--- KotlinGrammarTest/src/main/kotlin/com/terzeron/grammar/021FunctionalInterfaces.kt
+++ KotlinGrammarTest/src/main/kotlin/com/terzeron/grammar/021FunctionalInterfaces.kt
@@ -1,2 +1,33 @@
 package com.terzeron.grammar
 
+// functional interface or SAM(single abstract method) interface
+// 추상 멤버를 단 하나만 가짐. 다른 구상 멤버들은 가질 수 있음
+fun interface KRunnable {
+    fun invoke()
+}
+
+fun interface IntPredicate {
+    fun accept(i: Int): Boolean
+}
+
+fun functionalInterface1() {
+    // 일반적인 interface
+    val isEven1 = object : IntPredicate {
+        override fun accept(i: Int): Boolean {
+            return i % 2 == 0
+        }
+    }
+
+    // SAM
+    val isEven2 = IntPredicate { it % 2 == 0 }
+
+    println(isEven1.accept(3))
+    println(isEven1.accept(4))
+    println(isEven2.accept(5))
+    println(isEven2.accept(6))
+}
+
+fun main() {
+    println("---- functionalInterface1() ----")
+    functionalInterface1()
+}
