fb445639ce9bc70f307059dfc376bdb8b6d1f5e9
terzeron <terzeron@gmail.com>
2020-11-02 17:33:33 +0900

    add an example for sealed class

---
+++ KotlinGrammarTest/src/main/kotlin/com/terzeron/grammar/028SealedClass.kt
@@ -0,0 +1,24 @@
+package com.terzeron.grammar
+
+// 한정된 타입
+// 어떤 의미에서 enum 클래스의 확장이라고 볼 수 있음
+sealed class Expr
+data class Const(val number: Double) : Expr()
+data class Sum(var e1: Expr, val e2: Expr) : Expr()
+object NotANumber : Expr()
+
+fun sealedClassTest() {
+    fun eval(expr: Expr): Double = when (expr) {
+        is Const -> expr.number
+        is Sum -> eval(expr.e1) + eval(expr.e2)
+        NotANumber -> Double.NaN
+    }
+
+    val c: Const = Const(3.0)
+    println(eval(c));
+}
+
+fun main() {
+    println("---- sealedClassTest ----")
+    sealedClassTest()
+}
